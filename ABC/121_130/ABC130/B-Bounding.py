d6a109f46908a2d9c373491f5da0968bc1875959
Meteromio-pc <niihaao_nikoniko@yahoo.co.jp>
2019-07-04 00:21:24 +0900

    コードを短くしたいのでできるだけ修正した

--- ABC/121_130/ABC130/B-Bounding.py
+++ ABC/121_130/ABC130/B-Bounding.py
@@ -1,14 +1,8 @@
 N, X = map(int, input().split())
-L = [int(x) for x in input().split()]
+L = list(map(int, input().split()))
 D = []
-i = 0
-while N+1:
-  if i == 0:
-    D.append(0)
-  elif i<=N:
+D.append(0)
+for i in range(N+1):
+  if i >= 1 and i <= N:
     D.append(D[i-1]+L[i-1])
-  else:
-    break
-  i+=1
-#print(D)
-print(len([int(y) for y in D if int(y)<=X]))
+print(len([int(y) for y in D if int(y) <= X]))
