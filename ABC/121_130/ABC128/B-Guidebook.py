9263fe1d3327aaf744c85f8899f244047f2fb0cb
Meteromio-pc <niihaao_nikoniko@yahoo.co.jp>
2019-07-05 00:32:27 +0900

    code formated

--- ABC/121_130/ABC128/B-Guidebook.py
+++ ABC/121_130/ABC128/B-Guidebook.py
@@ -2,10 +2,10 @@ N = int(input())
 town_names = dict()
 assessments = []
 for x in range(N):
-  if x!=N:
+  if x != N:
     SP = input().split()
     assessments.append(SP[1])
-    town_names.setdefault(SP[0],[]).append(assessments.index(SP[1])+1)
+    town_names.setdefault(SP[0], []).append(assessments.index(SP[1])+1)
   else:
     break
 result = []
@@ -13,7 +13,7 @@ for t in sorted(town_names):
   tmp = []
   for pointer in town_names[t]:
     tmp.append(assessments[pointer-1])
-  for x in sorted(tmp, reverse=True,key=int):
+  for x in sorted(tmp, reverse=True, key=int):
     result.append(assessments.index(x)+1)
 for x in result:
   print(x)
